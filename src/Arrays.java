aaad8eaf85092f7a43fad3fcbb675675c8a47ba3
xialingfeng <lingfengxia.jake@gmail.com>
2022-09-11 16:10:25 -0400

    09/11/2022

---
+++ src/Arrays.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * A collection of Leetcode problems related to Arrays.
+ * @author xialingfeng
+ */
+public class Arrays {
+
+  /**.
+   * @leetcode 500: Keyboard Row.
+   * @Difficulty: Easy
+   * @OptimalComplexity: O(L) & O(26).
+   */
+  //Intuition
+  public String[] findWords(String[] words) {
+    List<String> ans = new ArrayList<>();
+    Set<Character> Up = new HashSet<>();
+    Set<Character> Mid = new HashSet<>();
+    Set<Character> Down = new HashSet<>();
+    String firstLine = "qwertyuiop";
+    String secondLine = "asdfghjkl";
+    String thirdLine = "zxcvbnm";
+    char[] temp1 = firstLine.toCharArray();
+    for (char chr : temp1) {
+      Up.add(chr);
+    }
+    char[] temp2 = secondLine.toCharArray();
+    for (char chr : temp2) {
+      Mid.add(chr);
+    }
+    char[] temp3 = thirdLine.toCharArray();
+    for (char chr : temp3) {
+      Down.add(chr);
+    }
+    for (String word : words) {
+      if (isValid(word, Up) || isValid(word, Mid) || isValid(word, Down)) {
+        ans.add(word);
+      }
+    }
+    String[] res = new String[ans.size()];
+    for (int i = 0; i < ans.size(); i++) {
+      res[i] = ans.get(i);
+    }
+    return res;
+  }
+
+  /**.
+   * @param str String
+   * @param set HashSet
+   * @return if all the characters can be found within set.
+   */
+  public boolean isValid(String str, Set<Character> set) {
+    for (int i = 0; i < str.length(); i++) {
+      if (!set.contains(Character.toLowerCase(str.charAt(i)))) {
+        return false;
+      }
+    }
+    return true;
+  }
+  //Best Solution
+
+  /**.
+   * Best solution with every character indexed
+   * @param words String[]
+   * @return String[]
+   */
+  public String[] OptimalFindWords(String[] words) {
+    List<String> list = new ArrayList<String>();
+    String rowIdx = "12210111011122000010020202";
+    for (String word : words) {
+      boolean isValid = true;
+      char idx = rowIdx.charAt(Character.toLowerCase(word.charAt(0)) - 'a');
+      for (int i = 1; i < word.length(); ++i) {
+        if (rowIdx.charAt(Character.toLowerCase(word.charAt(i)) - 'a') != idx) {
+          isValid = false;
+          break;
+        }
+      }
+      if (isValid) {
+        list.add(word);
+      }
+    }
+    String[] ans = new String[list.size()];
+    for (int i = 0; i < list.size(); ++i) {
+      ans[i] = list.get(i);
+    }
+    return ans;
+  }
+}
