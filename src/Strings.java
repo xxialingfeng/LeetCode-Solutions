14233e34af8c206947ae246821bcc028c812febc
xialingfeng <lingfengxia.jake@gmail.com>
2022-09-17 19:49:31 -0400

    09/17/2022

--- src/Strings.java
+++ src/Strings.java
@@ -21,4 +21,58 @@ public class Strings {
     }
     return true;
   }
+
+  /**.
+   * Leecode 521: longest uncommon subsequence I
+   * @Difficulty: Easy
+   * @OptimalComplexity: O(1) & O(1)
+   */
+  public int findLUSlength(String a, String b) {
+    if (a.equals(b)) {
+      return -1;
+    }
+    return Math.max(a.length(), b.length());
+  }
+
+  /**.
+   * Leetcode 522: longest uncommon subsequence II
+   * @Difficulty: Medium
+   * @OptimalComplexity: O(n2) & O(1)
+   * @param strs String[]
+   * @return int
+   */
+  public int findLUSlength(String[] strs) {
+    int res = -1;
+    for (int i = 0; i < strs.length; i++) {
+      boolean flag = false;
+      for (int j = 0; j < strs.length; j++) {
+        if (i == j) {
+          continue;
+        }
+        flag = flag || isSub(strs[i], strs[j]);
+      }
+      if (!flag) {
+        res = Math.max(res, strs[i].length());
+      }
+    }
+    return res;
+  }
+
+  /**
+   * Find out if string a is a subsequence of string b.
+   * @param a String
+   * @param b String
+   * @return boolean
+   */
+  public boolean isSub(String a, String b) {
+    int ps = 0;
+    int pb = 0;
+    while (ps < a.length() && pb < b.length()) {
+      if (a.charAt(ps) == b.charAt(pb)) {
+        ps++;
+      }
+      pb++;
+    }
+    return ps == a.length();
+  }
 }
