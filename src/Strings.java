eab3424cdc072373ab569084d1d8fa80c88847d2
xialingfeng <lingfengxia.jake@gmail.com>
2022-09-21 14:21:36 -0400

    09/21/2022

--- src/Strings.java
+++ src/Strings.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -110,5 +112,88 @@ public class Strings {
    */
   public String decode(String shortUrl) {
     return map535.get(shortUrl);
+  }
+
+  /**
+   * Leetcode 537: Complex Number Multiplication.
+   * @Difficulty: Medium
+   * @OptimalComplexity: O(n) & O(n)
+   * @param num1 String
+   * @param num2 String
+   * @return String
+   */
+  public String complexNumberMultiply(String num1, String num2) {
+    List<Character> list1 = new ArrayList<>();
+    List<Character> list2 = new ArrayList<>();
+    List<Character> list3 = new ArrayList<>();
+    List<Character> list4 = new ArrayList<>();
+    for (int i = 0; i < num1.length(); i++) {
+      if (num1.charAt(i) == '+') {
+        break;
+      }
+      list1.add(num1.charAt(i));
+    }
+    int left2 = 0;
+    while (num1.charAt(left2) != '+') {
+      left2++;
+    }
+    left2++;
+    while (left2 < num1.length()-1) {
+      list2.add(num1.charAt(left2));
+      left2++;
+    }
+    for (int i = 0; i < num2.length(); i++) {
+      if (num2.charAt(i) == '+') {
+        break;
+      }
+      list3.add(num2.charAt(i));
+    }
+    int left4 = 0;
+    while (num2.charAt(left4) != '+') {
+      left4++;
+    }
+    left4++;
+    while (left4 < num2.length()-1) {
+      list4.add(num2.charAt(left4));
+      left4++;
+    }
+    String ans = "";
+    ans += (getNum(list1) * getNum(list3) - getNum(list2) * getNum(list4)) +  "+" + (getNum(list1) * getNum(list4) + getNum(list2) * getNum(list3)) + "i";
+    return ans;
+  }
+
+  /**
+   * get int value. (Integer.parseInt)
+   * @param list List<Character>
+   * @return int
+   */
+  public int getNum(List<Character> list) {
+    int count = 1;
+    int sum = 0;
+    int left = 0;
+    if (list.get(0) == '-') {
+      count *= -1;
+      left++;
+    }
+    for (int i = list.size() - 1; i >= left; i--) {
+      sum += (list.get(i) - '0') * count;
+      count *= 10;
+    }
+    return sum;
+  }
+
+  /**
+   * Optimal Solution for leetcode 537.
+   */
+  public String complexNumberMultiplyOptimal(String num1, String num2) {
+    int a1 = Integer.parseInt(num1.substring(0,num1.indexOf('+')));
+    int a2 = Integer.parseInt(num1.substring(num1.indexOf('+') + 1,num1.length() - 1));
+    int b1 = Integer.parseInt(num2.substring(0,num2.indexOf('+')));
+    int b2 = Integer.parseInt(num2.substring(num2.indexOf('+') + 1,num2.length() - 1));
+
+    int aa = a1 * b1 - a2 * b2;
+    int bb = a2 * b1 + a1 * b2;
+
+    return aa + "+" + bb + "i" ;
   }
 }
