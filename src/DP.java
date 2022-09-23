11ce32d10572c9decadf6f24cf38857598344fda
xialingfeng <lingfengxia.jake@gmail.com>
2022-09-23 16:18:22 -0400

    09/23/2022

--- src/DP.java
+++ src/DP.java
@@ -35,4 +35,42 @@ public class DP {
     }
     return Arrays.stream(dp[key.length() - 1]).min().getAsInt();
   }
+
+  /**
+   * Leetcode 546: Remove Boxes.
+   * @Difficulty: Hard
+   * @OptimalComplexity: O(n4) & O(n3)
+   * @param boxes int[]
+   * @return int
+   */
+  public int removeBoxes(int[] boxes) {
+    int length = boxes.length;
+    int[][][] dp;
+    dp = new int[length][length][length];
+    return calculatePoints(dp, boxes, 0, length - 1, 0);
+  }
+
+  /**
+   * interval DP.
+   * @param dp int[][][]
+   * @param boxes int[]
+   * @param l int
+   * @param r int
+   * @param k int
+   * @return int
+   */
+  public int calculatePoints(int[][][] dp, int[] boxes, int l, int r, int k) {
+    if (l > r) {
+      return 0;
+    }
+    if (dp[l][r][k] == 0) {
+      dp[l][r][k] = calculatePoints(dp, boxes, l, r - 1, 0) + (k + 1) * (k + 1);
+      for (int i = l; i < r; i++) {
+        if (boxes[i] == boxes[r]) {
+          dp[l][r][k] = Math.max(dp[l][r][k], calculatePoints(dp, boxes, l, i, k + 1) + calculatePoints(dp, boxes, i + 1, r - 1, 0));
+        }
+      }
+    }
+    return dp[l][r][k];
+  }
 }
