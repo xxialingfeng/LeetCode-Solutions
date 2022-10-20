08f43c9fac3d5867dffb884476b9d8ae8dff0b81
xialingfeng <lingfengxia.jake@gmail.com>
2022-10-20 11:23:37 -0400

    10/20/2022

--- src/math.java
+++ src/math.java
@@ -22,4 +22,17 @@ public class math {
     }
     return sum == num;
   }
+
+  /**
+   * Leetcode 672 : Bulb Switcher II.
+   * @param n int
+   * @param presses int
+   * @return int
+   */
+  public int flipLights(int n, int presses) {
+    if (presses == 0) return 1;
+    if (n == 1) return 2;
+    if (n == 2) return (presses == 1) ? 3 : 4;
+    return (presses > 2) ? 8 : (presses == 1) ? 4 : 7;
+  }
 }
